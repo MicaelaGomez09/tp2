74f5507b8e1386a62bf97c5a6454e4f9d2e046bd
Micaela Gomez <92445801+MicaelaGomez09@users.noreply.github.com>
2022-01-03 03:02:06 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,25 +1,26 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 
-/*struct persona {
-    char nombre[50];
+#define MAX 20
+
+typedef struct{
+    char nombre [50] ;
+    char apellido[50];
     int edad;
     int dni;
-    int estado;
-};
- typedef struct persona persona;*/
 
- typedef struct {
+    int status;
+}nada;
 
-    char nombre[30];
-    int edad;
-    int dni;
-    int estado;
-}persona;
+//typedef struct nada nada ;
 
-void mostrarMenu();
-void ingreso(persona agenda[20]);
-void inicializarArray (persona agenda[20]);
-void mostrar(persona agenda[20]);
+void init(nada agenda[]);
+void printMenu();
+void alta(nada agenda[]);
+void baja(nada agenda[MAX]);
+void mostrar(nada agenda[MAX]);
+int buscar(nada agenda[MAX]);
+void ordenarNombre(nada agenda [MAX]);
+void Grafico(nada agenda [MAX]);
 
 #endif // FUNCIONES_H_INCLUDED
